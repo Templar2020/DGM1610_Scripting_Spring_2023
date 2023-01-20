92b6b4af6d8ee87031a2efbea834021c69359148
Preston Powell <preston.powell2020@gmail.com>
2023-01-19 17:41:02 -0700

    Added Variable and Move scripts

---
+++ First Prototype/Assets/Scripts/Variables.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // [Access Modifier] [Data-Type/ Type] [Identifier/Name] [Access Operator =] [Value]
+
+    public string herosName = "Batman";
+    private int herosAge = 30;
+
+    public float heroHeight = 6.2f;
+    private double heroNetWorth = 100.45d;
+
+    public char heroLetter = 'B';
+
+    public bool isHeroInv = false;
+
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
