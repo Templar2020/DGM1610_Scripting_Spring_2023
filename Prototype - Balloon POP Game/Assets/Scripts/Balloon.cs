48f43a7ec698ede643e6fa4a7d468193ae1d4a60
Preston Powell <preston.powell2020@gmail.com>
2023-02-02 17:27:33 -0700

    Completed Balloon POP Game

--- Prototype - Balloon POP Game/Assets/Scripts/Balloon.cs
+++ Prototype - Balloon POP Game/Assets/Scripts/Balloon.cs
@@ -6,12 +6,15 @@ public class Balloon : MonoBehaviour
 {
     public int clickToPop = 3; // How clicks before the balloon pops
     public float scaleToIncrease = 0.10f; // Scale increase each time the balloon is clicked
+    public ScoreManager scoreManager; // A reference to the score manager
+    public int scoreToGive = 100;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       //Get ScoreManager Component
+       scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>(); 
     }
 
     void OnMouseDown()
@@ -22,6 +25,8 @@ public class Balloon : MonoBehaviour
        // Check to see if clickToPop has reached zero. Check to see if the balloon pops.
        if(clickToPop == 0)
        {
+           //Tell the scoremanager to increase the score by a certain amount
+           scoreManager.IncreaseScoreText(scoreToGive);
            Destroy(gameObject); //Destroy and remove popped balloon
        }
     }
--- Prototype - Balloon POP Game/Assets/Scripts/MoveUp.cs
+++ Prototype - Balloon POP Game/Assets/Scripts/MoveUp.cs
@@ -6,11 +6,15 @@ public class MoveUp : MonoBehaviour
 {
     public float moveSpeed = 1.0f;
     public float upperBound = 15;
+    public ScoreManager scoreManager; // Stores a reference to the scoremanager
+    public Balloon balloon; // Reference the balloon script to get score
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+        balloon = GetComponent<Balloon>();
     }
 
     // Update is called once per frame
@@ -21,7 +25,8 @@ public class MoveUp : MonoBehaviour
 
         if(transform.position.y > upperBound)
         {
-            Destroy(gameObject);
+            scoreManager.DecreaseScoreText(balloon.scoreToGive);//Reduces score for allowing the balloon to leave the screen
+            Destroy(gameObject); // Pops the balloon
         }
     }
 }
